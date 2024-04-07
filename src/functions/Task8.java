4f74cc80a734470ced2a7c8ba22a1ffc25e06f28
quemin2402 <mereyibraim2006@gmail.com>
2024-04-07 22:49:15 +0500

    Solution for the eighth task

---
+++ src/functions/Task8.java
@@ -0,0 +1,22 @@
+package functions;
+
+import java.util.Scanner;
+
+public class Task8 {
+    public static void checkDigitsInString(Scanner sc) {
+        sc.nextLine();
+        System.out.println("Enter a string. Does it include only digits?");
+        String s = sc.nextLine();
+        System.out.println(checkDigits(s, 0) ? "Yes" : "No");
+    }
+
+    private static boolean checkDigits(String s, int i) {
+        if (i == s.length())
+            return true;
+        char currentSymbol = s.charAt(i);
+        if (!Character.isDigit(currentSymbol)) {
+            return false;
+        }
+        return checkDigits(s, i + 1);
+    }
+}
