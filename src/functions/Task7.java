1ca58abf2a82ecba70af81fc813d48e5dc92dfa4
quemin2402 <mereyibraim2006@gmail.com>
2024-04-07 22:48:54 +0500

    Solution for the seventh task

---
+++ src/functions/Task7.java
@@ -0,0 +1,20 @@
+package functions;
+
+import java.util.Scanner;
+
+public class Task7 {
+    public static void reverseArray(Scanner sc) {
+        System.out.println("Enter the number of elements:");
+        int n = sc.nextInt();
+        System.out.println("Enter the elements:");
+        reversingArray(sc, n);
+        System.out.println();
+    }
+    private static void reversingArray(Scanner sc, int n) {
+        if (n == 0)
+            return;
+        int element = sc.nextInt();
+        reversingArray(sc, n - 1);
+        System.out.print(element + " ");
+    }
+}
